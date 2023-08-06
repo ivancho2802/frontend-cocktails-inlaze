364b749998b62ebe0eedfb6d97a4df0b19dd1d8b
ivan diaz <idiaz@scharfteinlabs.com>
2023-08-05 23:13:10 -0500

    se agregan la url de la api y datos globales de coonfiguracion del sitio

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,1 +1,35 @@
-export let urlApi = "https://www.thecocktaildb.com/";
+export let urlApi = "https://www.thecocktaildb.com/api/json/v1/";
+export let urlApiIngredientesImages = "https://www.thecocktaildb.com/images/ingredients/";
+
+export let key = "1";
+
+export let idDummy = "11410";
+export let descriptionDummy = "Gin, also known as gin, is one of the most popular and appreciated alcoholic beverages worldwide, especially for its versatility in cocktails. Originally from Europe, this spirit has evolved over the centuries and comes in a wide range of styles and flavors."
+export let strDrinkThumb = "../../../assets/cocktails/Botella-Gin-centro.png"
+export let dataDummy = {
+    idDummy,
+    descriptionDummy,
+    strDrinkThumb
+};
+
+export let nameImagesBackgrgound = [
+    "fizz-fondo.png",
+    "Fondo-limon.png",
+    "gin-fondo.png",
+    "Limon-amarillo-fondo.png",
+    "Rhum-fondo.png",
+    "miel-fondo.png",
+    "tonic-fondo.png",
+];
+
+export const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+export const languages = [
+    'EN',
+    'ES',
+    'DE',
+    'FR',
+    'IT',
+    //'ZH-HANS',
+    //'ZH-HANT'
+]
